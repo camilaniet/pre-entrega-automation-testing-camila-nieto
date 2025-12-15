1d212baa8cf9de48b645a4a2b64117205581d5b7
camilaniet@gmail.com <camilaniet@gmail.com>
2025-12-14 21:34:13 -0300

    Commit 3

--- conftest.py
+++ conftest.py
@@ -1,10 +1,14 @@
 import pytest
 from selenium import webdriver
-
+from pages.login_page import LoginPage
 
 @pytest.fixture
 def driver():
-    driver = webdriver.Edge()
-    driver.maximize_window()
+    driver = webdriver.Chrome()
     yield driver
     driver.quit()
+
+@pytest.fixture
+def login_in_driver(driver,usuario,password):
+    LoginPage(driver).abrir_pagina().login_completo(usuario,password)
+    return driver
